e1fbddcb45450ea44f0c75cc0fb81dfb32ef63e4
joelei <joel.einarsson@hotmail.com>
2026-01-30 15:56:10 +0100

    Small tweak

--- Car.java
+++ Car.java
@@ -2,8 +2,8 @@ import java.awt.*;
 
 public abstract class Car implements Movable {
 
-    private final int nrDoors; // Number of doors on the car
-    private final double enginePower; // Engine power of the car
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
     private double currentSpeed; // The current speed of the car
     private Color color; // Color of the car
     private String modelName; // The car model name
