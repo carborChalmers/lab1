3368eb2971449a527f205d4acb753ed665cad88f
carborChalmers <carbor@chalmers.se>
2026-02-11 16:08:34 +0100

    Update Saab95.java

--- src/app/Saab95.java
+++ src/app/Saab95.java
@@ -1,14 +1,15 @@
 package app;
 import java.awt.*;
 
-public class Saab95 extends Vehicle {
+public class Saab95 extends Vehicle implements Transportable{
 
     private boolean turboOn;
-
+    private boolean BeingTransported;
     
     public Saab95(){
         super(2, Color.red, 125, "Saab95",1500);
 	    turboOn = false;
+        BeingTransported = false;
     }
     
 
@@ -26,5 +27,13 @@ public class Saab95 extends Vehicle {
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return getEnginePower() * 0.01 * turbo;
+    }
+    @Override
+    public boolean isBeingTransported(){
+        return BeingTransported;
+    }
+    @Override
+    public void setBeingTransported(boolean transported){
+        this.BeingTransported=transported;
     }
 }
